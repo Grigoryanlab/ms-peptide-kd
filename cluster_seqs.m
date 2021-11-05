8bc54444b5b7325b95f76724894a0a1143a091d8
Gevorg Grigoryan <gevorg.grigoryan@gmail.com>
2021-11-05 17:44:14 -0400

    Added a sequence clustering script

---
+++ cluster_seqs.m
@@ -0,0 +1,23 @@
+% read sequences and the substitution matrix
+F = fastaread('peptides.fasta');
+alpha = 'ACDEFGHIKLMNPQRSTVWY';
+B = blosum(65, 'Order', alpha);
+
+% encode sequences via substitution-matrix rows for each amino acid
+S = zeros(length(F), length(F(1).Sequence)*length(alpha));
+G = cell(length(F), 1); % groupings by the P(0) amino acid
+for i = 1:length(F)
+    for j = 1:length(F(i).Sequence)
+        aaj = find(alpha == F(i).Sequence(j), 1);
+        S(i, length(alpha)*(j-1)+1:length(alpha)*j) = B(aaj, :);
+    end
+    G{i} = F(i).Sequence(end);
+end
+
+% do a t-SNE plot
+Y = tsne(S);
+gscatter(Y(:,1), Y(:,2), G);
+
+% make it pretty
+h = gscatter(Y(:,1), Y(:,2), G, [], '.', 12);
+set(gca, 'FontSize', 16);
